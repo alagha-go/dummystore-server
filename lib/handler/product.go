10ab7a86b07dc0ee32e2f8b5b7e587e542e5d8eb
Ubuntu <ubuntu@interphlix.com>
2022-03-19 15:03:21 +0000

    this file handle all http request and response  related to the products package

---
+++ lib/handler/product.go
@@ -0,0 +1,60 @@
+package handler
+
+import (
+	p"dummystore/lib/commerce/products"
+	"encoding/json"
+	"net/http"
+)
+
+
+func GetProductByAttribute(res http.ResponseWriter, req *http.Request) {
+	res.Header().Add("content-type", "application/json")
+	var status int
+	if req.Method != "GET" {
+		res.WriteHeader(http.StatusBadGateway)
+		return
+	}
+	var data p.Product
+	var err error
+	id := req.URL.Query().Get("id")
+	asin := req.URL.Query().Get("asin")
+
+	if id != "" {
+		data, err, status = p.GetProductByID(id)
+		res.WriteHeader(status)
+	}else if asin != ""{
+		data, err, status = p.GetProductByAsin(asin)
+		res.WriteHeader(status)
+	}else {
+		json.NewEncoder(res).Encode(Error{Error: "provide a product id or asin"})
+		return
+	}
+
+	if err != nil {
+		json.NewEncoder(res).Encode(Error{Error: err.Error()})
+		return
+	}
+	encoder := json.NewEncoder(res)
+	encoder.SetEscapeHTML(false)
+	encoder.Encode(data)
+}
+
+
+func UpdateOneProduct(res http.ResponseWriter, req *http.Request) {
+	res.Header().Add("content-type", "application/json")
+	if req.Method != "PUT" || req.Method != "UPDATE" {
+		res.WriteHeader(http.StatusBadGateway)
+		return
+	}
+	var product p.Product
+	err := json.NewDecoder(req.Body).Decode(&product)
+	if err != nil {
+		json.NewEncoder(res).Encode(Error{Error: "could not unmarshal data"})
+		return
+	}
+	data, status := p.UpdateOneProduct(product)
+	res.WriteHeader(status)
+	encoder := json.NewEncoder(res)
+	encoder.SetEscapeHTML(false)
+	encoder.Encode(data)
+}
