45455a672983d0afd4a625b2dc8ddf5a8f09cc3a
Ubuntu <ubuntu@interphlix.com>
2022-03-23 07:14:54 +0000

    added route handler for own a product and add a new product

--- lib/handler/product.go
+++ lib/handler/product.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
-	p"dummystore/lib/commerce/products"
+	p "dummystore/lib/commerce/products"
 	"encoding/json"
 	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 
@@ -57,4 +59,69 @@ func UpdateOneProduct(res http.ResponseWriter, req *http.Request) {
 	encoder := json.NewEncoder(res)
 	encoder.SetEscapeHTML(false)
 	encoder.Encode(data)
+}
+
+
+func AddNewProduct(res http.ResponseWriter, req *http.Request) {
+	res.Header().Add("content-type", "application/json")
+	if req.Method != "POST" {
+		res.WriteHeader(http.StatusBadGateway)
+		return
+	}
+	user, status, err := VerifyUser(req)
+	if err != nil {
+		res.WriteHeader(status)
+		json.NewEncoder(res).Encode(Error{Error: err.Error()})
+		return
+	}
+	var product p.Product
+
+	err = json.NewDecoder(req.Body).Decode(&product)
+
+	if err != nil {
+		json.NewEncoder(res).Encode(Error{Error: "invalid json"})
+	}
+
+	product.OwnerID = user.ID
+
+	product, status, err = p.AddNewProduct(product)
+	res.WriteHeader(status)
+	if err != nil {
+		json.NewEncoder(res).Encode(Error{Error: err.Error()})
+		return
+	}
+	encoder := json.NewEncoder(res)
+	encoder.SetEscapeHTML(false)
+	encoder.Encode(product)
+}
+
+func OwnProduct(res http.ResponseWriter, req *http.Request) {
+	res.Header().Add("content-type", "application/json")
+	if req.Method != "GET" {
+		res.WriteHeader(http.StatusBadGateway)
+		return
+	}
+
+	user, status, err := VerifyUser(req)
+	if err != nil {
+		res.WriteHeader(status)
+		json.NewEncoder(res).Encode(Error{Error: err.Error()})
+		return
+	}
+
+	id := req.URL.Query().Get("id")
+	ID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		res.WriteHeader(400)
+		json.NewEncoder(res).Encode(Error{Error: "invalid id"})
+		return
+	}
+
+	product, status, err := p.OwnProduct(ID, user.ID)
+	res.WriteHeader(status)
+	if err != nil {
+		json.NewEncoder(res).Encode(Error{Error: err.Error()})
+		return
+	}
+	json.NewEncoder(res).Encode(product)
 }
