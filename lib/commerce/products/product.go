2e6dbc294eb018f7e344cbb72b01826253bb128f
Ubuntu <ubuntu@interphlix.com>
2022-03-23 07:11:00 +0000

    added  functions to add new product and to own an existing product

--- lib/commerce/products/product.go
+++ lib/commerce/products/product.go
@@ -48,3 +48,44 @@ func GetProductByAsin(asin string) (Product, int, error) {
 	
 	return product, 200, nil
 }
+
+
+func AddNewProduct(product Product) (Product, int, error) {
+	collection := v.Client.Database("Dummystore").Collection("Products")
+	ctx := context.Background()
+	product.ID = primitive.NewObjectID()
+
+	_, err := collection.InsertOne(ctx, product)
+	if err != nil {
+		return Product{}, 500, v.DatabaseCouldNotSave
+	}
+	collection.FindOne(ctx, bson.M{"_id": product.ID}).Decode(&product)
+	return product, 201, nil
+}
+
+
+func OwnProduct(productID, userID primitive.ObjectID) (Product, int, error) {
+	var product Product
+	collection := v.Client.Database("Dummystore").Collection("Products")
+	ctx := context.Background()
+
+	err := collection.FindOne(ctx, bson.M{"_id": productID}).Decode(&product)
+	if err != nil {
+		return product, 404, v.ProductDoesNotExist
+	}
+
+	if product.OwnerID.Hex() != "000000000000000000000000" {
+		return product, 400, errors.New("Product is owned by someone else")
+	}
+
+	filter := bson.M{"_id": bson.M{"$eq": product.ID}}
+	update := bson.M{"$set": bson.M{"owner_id": userID}}
+
+	_, err = collection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return product, 404, v.CouldNotUpdateData
+	}
+
+	collection.FindOne(ctx, bson.M{"_id": productID}).Decode(&product)
+	return product, 200, nil
+}
