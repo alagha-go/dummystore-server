78bcde6da7380ec0bbaf2d81bbcf2f0545cc17fe
Ubuntu <ubuntu@interphlix.com>
2022-03-24 04:28:41 +0000

    added handler to handle on call for get products with deparmentID

--- lib/handler/departments.go
+++ lib/handler/departments.go
@@ -2,8 +2,11 @@ package handler
 
 import (
 	"dummystore/lib/commerce/dep"
+	"dummystore/lib/commerce/products"
 	"encoding/json"
 	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 
@@ -16,11 +19,35 @@ func GetAllDepartments(res http.ResponseWriter, req *http.Request) {
 	data, status, err := dep.GetAllDepartments()
 	res.WriteHeader(status)
 	if err != nil {
-		res.WriteHeader(http.StatusInternalServerError)
+		res.WriteHeader(status)
 		json.NewEncoder(res).Encode(Error{Error: err.Error()})
 		return
 	}
 	encoder := json.NewEncoder(res)
 	encoder.SetEscapeHTML(false)
 	encoder.Encode(data)
+}
+
+
+func GetProductsByDepartment(res http.ResponseWriter, req *http.Request) {
+	res.Header().Add("content-type", "application/json")
+	if req.Method != "GET" {
+		res.WriteHeader(405)
+		return
+	}
+	id := req.URL.Query().Get("id")
+	ID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		res.WriteHeader(400)
+		json.NewEncoder(res).Encode(Error{Error: "invalid id"})
+		return
+	}
+
+	data, status, err := products.GetProductByDepartment(ID)
+	res.WriteHeader(status)
+	if err != nil {
+		json.NewEncoder(res).Encode(Error{Error: err.Error()})
+		return
+	}
+	json.NewEncoder(res).Encode(data)
 }
